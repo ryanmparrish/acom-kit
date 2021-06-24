a660164371c17380de17482661d3cd3facf73672
Ryan Parrish <thi64146@adobe.com>
2021-06-24 09:04:08 -0600

    dark and unique tab ids

--- blocks/tabs/scripts.js
+++ blocks/tabs/scripts.js
@@ -13,25 +13,38 @@ const init = (element) => {
     const tabsListContainer = document.createElement('div');
     const tabs = element.querySelectorAll(':scope > div:nth-of-type(2) > div');
     const tabsContent = element.querySelectorAll(':scope > div:nth-of-type(3) > div');
-    const tabsStuff = element.querySelectorAll(':scope > div:nth-of-type(4) > div');
+    const tabIds = element.querySelectorAll(':scope > div:nth-of-type(4) > div');
+    const uniqueId = Date.now();
     tabs.forEach(function (tab, i) {
         tab.remove();
         let isChecked = i === 0 ? ' checked' : '';
-        let radioLabel = '<input class="radiotab" name="tabs" tabindex="1" type="radio" id="tab-' + i + '"' + isChecked +' />'
-        + '<label class="label" for="tab-' + i + '">' + tab.textContent + '</label>';
+        let radioLabel = '<input class="radiotab" name="tabs" tabindex="1" type="radio" id="tab-' + i + '-' + uniqueId + '"' + isChecked + ' />'
+        + '<label class="label" for="tab-' + i + '-' + uniqueId + '">' + tab.textContent + '</label>';
         tabsListContainer.insertAdjacentHTML('beforeend', radioLabel );
     });
+
+    tabIds.forEach(function (id, i) {
+        let assocTabContent = element.querySelector('.tabid-' + id.textContent);
+        let tabList = tabsListContainer.querySelector('label[for="tab-' + i + '-' + uniqueId + '"]');
+//        console.log(assocTabContent, tabList, '.tabid-'+id.textContent);
+//        if(assocTabContent && tabList) {
+//            assocTabContent.classList.add('tab-content', 'tab-content-'+i, 'tab-content--assoc');
+//            assocTabContent.setAttribute('tabindex', 1);
+//            tabList.parentNode.insertBefore(assocTabContent, tabList.nextSibling);
+//        }
+    });
+
     tabsContent.forEach(function (content, i) {
         content.classList.add('tab-content', 'tab-content-'+i);
         content.setAttribute('tabindex', 1);
-        var tabList = tabsListContainer.querySelector('label[for="tab-' + i + '"]');
+        var tabList = tabsListContainer.querySelector('label[for="tab-' + i + '-' + uniqueId + '"]');
         tabList.parentNode.insertBefore(content, tabList.nextSibling);
     });
 
     // append the tabs list container after tabListBg
     tabsListContainer.classList.add('container', 'list-container');
     tabListBg.after(tabsListContainer);
-    tabsStuff.innerHTML = '';
+    tabIds.innerHTML = '';
 };
 
 export default init;
