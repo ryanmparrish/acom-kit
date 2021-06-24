ed1e3e3ed79b774deab8e4544893b338a6b042c1
Ryan Parrish <thi64146@adobe.com>
2021-06-24 12:27:31 -0600

    changed the logic on tab content assoc to it could be it's own obj. marquee and tabContent

--- blocks/tabs/scripts.js
+++ blocks/tabs/scripts.js
@@ -12,33 +12,25 @@ const init = (element) => {
     // create the tabs list container div
     const tabsListContainer = document.createElement('div');
     const tabs = element.querySelectorAll(':scope > div:nth-of-type(2) > div');
-    const tabsContent = element.querySelectorAll(':scope > div:nth-of-type(3) > div');
-    const tabIds = element.querySelectorAll(':scope > div:nth-of-type(4) > div');
-    const uniqueId = Date.now();
+    const tabIds = element.querySelectorAll(':scope > div:nth-of-type(3) > div');
     tabs.forEach(function (tab, i) {
         tab.remove();
         let isChecked = i === 0 ? ' checked' : '';
-        let radioLabel = '<input class="radiotab" name="tabs" tabindex="1" type="radio" id="tab-' + i + '-' + uniqueId + '"' + isChecked + ' />'
-        + '<label class="label" for="tab-' + i + '-' + uniqueId + '">' + tab.textContent + '</label>';
+        let radioLabel = '<input class="radiotab" name="tabs" tabindex="1" type="radio" id="tab-' + tabIds[i].textContent + '"' + isChecked + ' />'
+        + '<label class="label" for="tab-' + tabIds[i].textContent + '">' + tab.textContent + '</label>';
         tabsListContainer.insertAdjacentHTML('beforeend', radioLabel );
     });
 
     tabIds.forEach(function (id, i) {
-        let assocTabContent = element.querySelector('.tabid-' + id.textContent);
-        let tabList = tabsListContainer.querySelector('label[for="tab-' + i + '-' + uniqueId + '"]');
-//        console.log(assocTabContent, tabList, '.tabid-'+id.textContent);
-//        if(assocTabContent && tabList) {
-//            assocTabContent.classList.add('tab-content', 'tab-content-'+i, 'tab-content--assoc');
-//            assocTabContent.setAttribute('tabindex', 1);
-//            tabList.parentNode.insertBefore(assocTabContent, tabList.nextSibling);
-//        }
-    });
-
-    tabsContent.forEach(function (content, i) {
-        content.classList.add('tab-content', 'tab-content-'+i);
-        content.setAttribute('tabindex', 1);
-        var tabList = tabsListContainer.querySelector('label[for="tab-' + i + '-' + uniqueId + '"]');
-        tabList.parentNode.insertBefore(content, tabList.nextSibling);
+        id.remove();
+        let assocTabContent = document.getElementsByClassName('tab-id-' + id.textContent)[0];
+        let tabList = tabsListContainer.querySelector('label[for="tab-' + id.textContent + '"]');
+        console.log(assocTabContent, tabList);
+        if(assocTabContent && tabList) {
+            assocTabContent.classList.add('tab-content', 'tab-index-'+i);
+            assocTabContent.setAttribute('tabindex', 1);
+            tabList.parentNode.insertBefore(assocTabContent, tabList.nextSibling);
+        }
     });
 
     // append the tabs list container after tabListBg
