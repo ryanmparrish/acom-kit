143b103a54c8d06f9ccb4c81636acc5b23134a6d
Ryan Parrish <thi64146@adobe.com>
2021-06-25 13:01:24 -0600

    better support for outro block

--- blocks/merch/scripts.js
+++ blocks/merch/scripts.js
@@ -2,6 +2,9 @@
 // Merch Block
 
 const init = (element) => {
+    console.log(element.classList);
+    const typeIntro = element.classList.contains('intro');
+    console.log('typeIntro', typeIntro);
     const ribbon = element.querySelector(':scope > div:first-of-type > div');
     ribbon.classList.add('ribbon');
     const bgImg = ribbon.querySelector(':scope img');
@@ -9,7 +12,7 @@ const init = (element) => {
         const bgColor = ribbon.textContent;
         ribbon.style = `background: ${bgColor}`;
         ribbon.innerHTML = '';
-        element.style.background = "#000000";
+//        element.style.background = "#000000";
     }
     const title = element.querySelector(':scope > div:nth-of-type(2)');
     title.classList.add('title');
@@ -20,6 +23,13 @@ const init = (element) => {
     const prods = element.querySelectorAll(':scope > div:nth-of-type(3) > div');
     prods.forEach(function (prod, index) {
         prod.classList.add('product-wrapper');
+
+        // polyfill missing logo div for styling
+        if(typeIntro) {
+            let stub = document.createElement('div');
+            stub.classList.add('stub');
+            prod.insertBefore(stub, prod.firstChild);
+        }
         dv.appendChild(prod);
     });
     // append the container div after title
