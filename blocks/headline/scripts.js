6c89569e4e1fe9addcd02529d3a9c7cce3b89946
Ryan Parrish <thi64146@adobe.com>
2021-08-20 14:34:02 -0600

    Quick stub in headline styles

--- blocks/headline/scripts.js
+++ blocks/headline/scripts.js
@@ -3,6 +3,16 @@ const init = (element) => {
 
     const container = element.querySelector(':scope > div');
     container.classList.add('container');
+
+    const ribbon = element.querySelector(':scope > div:last-of-type > div');
+    ribbon.classList.add('ribbon');
+    const bgImg = ribbon.querySelector(':scope img');
+    if (!bgImg) {
+        const bgColor = ribbon.textContent;
+        ribbon.style = `background: ${bgColor}`;
+        ribbon.innerHTML = '';
+    }
+
 };
 
 export default init;
